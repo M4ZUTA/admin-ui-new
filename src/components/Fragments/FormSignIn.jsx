3dc00fc319d96be367043babf1379f4f28761725
M4ZUTA <marioignatius25@gmail.com>
2025-12-18 23:17:55 +0700

    Reusable Component

---
+++ src/components/Fragments/FormSignIn.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import LabeledInput from "../Elements/LabeledInput";
+import CheckBox from "../Elements/CheckBox";
+import Button from "../Elements/Button";
+
+function FormSignIn() {
+    return (
+        <>
+            {/* form start */}
+        <div className="mt-16">
+          <form action="">
+            <div className="mb-6">
+              <LabeledInput />
+            </div>
+            <div className="mb-6">
+                <LabeledInput />
+            </div>
+            <div className="mb-3">
+                <CheckBox />
+            </div>
+            <Button />
+          </form>
+        </div>
+        {/* form end */}
+        {/* teks start */}
+        <div className="my-9 px-7 flex flex-col justify-center items-center text-xs text-gray-03">
+          <div className="border border-gray-05 w-full"></div>
+          <div class="px-2 bg-special-mainBg absolute"> or sign in with</div>
+        </div>
+        {/* teks end */}
+        {/* sign in with google start */}
+        <div className="mb-8">
+          <Button />
+        </div>
+        {/* sign in with google end */}
+        {/* link start */}
+        <div className="flex justify-center">
+          <a className="text-primary text-sm font-bold">Create an account</a>
+        </div>
+        {/* link end */}
+        </>
+    );
+}
+
+export default FormSignIn;

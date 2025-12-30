a8b755d9943dbe434bd0cc38f1882e5db134f610
M4ZUTA <marioignatius25@gmail.com>
2025-12-30 08:00:54 +0700

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import SignInPage from "./pages/signIn.jsx";
 import SignUpPage from "./pages/signUp.jsx";
-import error from "./pages/error.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./pages/error.jsx";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 
 function App() {
   const myRouter = createBrowserRouter([
@@ -11,14 +11,14 @@ function App() {
       element: (
         <div className="flex justify-center items-center min-h-screen">
           <Link to="/login" className="p-2 m-5 bg-primary text-white">
-            Login
+            <div>Login</div>
           </Link>
           |
           <Link to="/register" className="p-2 m-5 bg-primary text-white">
-            Register
+            <div>Register</div>
           </Link>
         </div>),
-      errorElement: <error />,
+      errorElement: <Error />,
     },
     {
       path: "/login",
